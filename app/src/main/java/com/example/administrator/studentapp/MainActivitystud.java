046ff8f242b46c33450619763fa033f210f56785
athirajayachandren <athirajayanvava1998@gmail.com>
2019-01-24 16:06:14 +0530

    loginbutton

--- app/src/main/java/com/example/administrator/studentapp/MainActivitystud.java
+++ app/src/main/java/com/example/administrator/studentapp/MainActivitystud.java
@@ -1,6 +1,7 @@
 package com.example.administrator.studentapp;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,7 +13,7 @@ public class MainActivitystud extends AppCompatActivity {
 
     EditText s1,s2;
     Button b1,b2;
-    String getsid,getpassword;
+    String getsid,getpassword,checkusername;
     String id= "mzc";
     String pass= "college";
 
@@ -21,6 +22,17 @@ public class MainActivitystud extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_activitystud);
+
+
+        SharedPreferences pref=getSharedPreferences("Login",MODE_PRIVATE);
+        checkusername=pref.getString("studentid",null);
+
+        if(checkusername!= null)
+        {
+            Intent j=new Intent(getApplicationContext(),welcomeActivity.class);
+            startActivity(j);
+        }
+
 
         s1=(EditText)findViewById(R.id.stuid);
         s2=(EditText)findViewById(R.id.pass);
@@ -36,7 +48,15 @@ public class MainActivitystud extends AppCompatActivity {
 
                 if ((getsid .equals(id)) && (getpassword .equals(pass))) {
 
+                    SharedPreferences.Editor editor=getSharedPreferences("Login",MODE_PRIVATE).edit();
+                    editor.putString("studentid",getsid);
+                    editor.putString("password",getpassword);
+                    editor.apply();
+
                     Intent j = new Intent(getApplicationContext(), welcomeActivity.class);
+                   j.putExtra("student",getsid);
+                   j.putExtra("password",getpassword);
+
                     startActivity(j);
                 } else {
                     Toast.makeText(getApplicationContext(), "user name and password are incorrect", Toast.LENGTH_LONG).show();
--- app/src/main/java/com/example/administrator/studentapp/welcomeActivity.java
+++ app/src/main/java/com/example/administrator/studentapp/welcomeActivity.java
@@ -1,13 +1,51 @@
 package com.example.administrator.studentapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class welcomeActivity extends AppCompatActivity {
+    String sid,pass,chackuser;
+   String getsid,getPass;
+   Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        b=(Button)findViewById(R.id.logout);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor=getSharedPreferences("Login",MODE_PRIVATE).edit();
+                editor.clear();
+                editor.commit();
+
+                Intent j=new Intent(getApplicationContext(),MainActivitystud.class);
+                startActivity(j);
+            }
+        });
+
+        SharedPreferences sharedPreferences=getSharedPreferences("Login",MODE_PRIVATE);
+       getsid=sharedPreferences.getString("studentid",null);
+       getPass=sharedPreferences.getString("password",null);
+        Toast.makeText(getApplicationContext(),getsid,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(),getPass,Toast.LENGTH_LONG).show();
+
+
+
+//        Intent j=getIntent();
+//        sid=j.getStringExtra("studentid");
+//        pass=j.getExtras().getString("password");
+
+//        Toast.makeText(getApplicationContext(),sid,Toast.LENGTH_LONG).show();
+//        Toast.makeText(getApplicationContext(),pass,Toast.LENGTH_LONG).show();
+
     }
 }
